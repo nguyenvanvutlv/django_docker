5c8df4c91e3c79d9e88aab2859e6acb5da8a2cb3
nguyenvanvutlv <nguyenvanvu.tlv@gmail.com>
2023-05-08 18:02:38 +0700

    UP file compatibility testing using selenium

--- system/settings.py
+++ system/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "haui_project",
-        "USERNAME" : "root",
+        "USER" : "root",
         "PASSWORD" : "root",
         "HOST": "127.0.0.1",
         "PORT": "3306",
